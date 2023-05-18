4c6221fa3142120120264bc5249a6101c0630ad7
Rafli Muadzan <raflimuadzan2@gmail.com>
2023-05-18 16:30:22 +0800

    add foreign key constraint for album_id in table songs

---
+++ migrations/1684398013387_add-foreign-key-to-albumid-column-in-songs-table.js
@@ -0,0 +1,15 @@
+exports.up = (pgm) => {
+  pgm.sql("INSERT INTO albums(id, name, year, created_at, updated_at) VALUES ('old_songs', 'old_songs', 0, '0000-05-18T07:12:22.062Z', '0000-05-18T07:12:22.062Z')");
+
+  pgm.sql("UPDATE songs SET album_id = 'old_songs' WHERE album_id IS NULL");
+
+  pgm.addConstraint('songs', 'fk_songs.album_id_album.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('songs', 'fk_songs.album_id_albums.id');
+
+  pgm.sql("UPDATE songs SET album_if = NULL WHERE album_id = 'old_songs'");
+
+  pgm.sql("DELETE FROM albums WHERE id = 'old_songs'");
+};
